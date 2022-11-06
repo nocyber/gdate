f940739e7907802331113e1399374e4d5de49fd3
Dan <107225636+SammyGoogles@users.noreply.github.com>
2022-11-06 08:14:30 -0500

    Version 0.93

--- gdate.sh
+++ gdate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-VERSION='v0.91.9'
+VERSION='v0.93'
+# @author Daniel Lagacé
+# @license GNE GPLv2
+
 
 # To add a zone,
 # 1) use "timedatectl list-timezones" to find a timezone.
@@ -7,14 +10,12 @@ VERSION='v0.91.9'
 # 3) add the Unix city or timezone code you found in step 1 into the "TIMEZONE" array.
 # (The NAME and TIMEZONE arrays have to be in the same order)
 
-
-
+# Local Variables
 CURRENT_CITY='Montréal'                           # Any Name
-CURRENT_TIMEZONE[0]='America/New_York'            # ET  / Unix Timezone
+CURRENT_TIMEZONE[0]='America/Toronto'             # ET  / Local / Unix Timezone
 CURRENT_TIMEZONE[1]='EST'                         # EST / Unix Timezone
 CURRENT_TIMEZONE[2]='EDT4'                        # EDT / Unix Timezone
-NEXT_TZ_CHANGE[0]="Next Local Time change: [EDT(UTC-4) -> EST(UTC-5)]"      # This message does not automatically change
-NEXT_TZ_CHANGE[1]='Sun 06 Nov 2022 02:00:00 AM EDT'                         # This message does not automatically change
+
 
 # Colors
 #### 8-bit ANSI color codes reference:
@@ -26,26 +27,25 @@ M='\e[0;31m'  # Montreal (or Main city)
 C='\e[0;36m'  # commands
 P='\e[0;35m'  # Purple
 X='\e[0m'     # default terminal color.
-
 BAR='\e[0m---------------------------------------------------------------------------------------\n'
 
 
-# Variables
+# World Time Variables
 NAME=( \
-"${D}""Sydney, Australia  [Spring=Sept 21, DST starts in Oct.]" \
-"${S}"'UTC+9    Japan (no DST)' \
-"${S}"'UTC+8:   China/HK, Singapore, Philipines (no DST)' \
+"${D}""Sydney, Australia  [Spring=Sept 21]" \
+"${S}"'UTC+9    Japan' \
+"${S}"'UTC+8:   China/HK, Singapore, Philipines' \
 "${S}"'UTC+5.5: India  (no DST)' \
 "${D}"'Israel' \
 "${D}"'Paris, Barcelona' \
 "${D}"'London' \
 "${S}"'UTC' \
-"${S}"'UTC-3    Sao Paolo [Brasilia Time] (no DST)' \
+"${S}"'UTC-3    Sao Paolo [Brasilia Time]' \
 "${M}"'New York, Montréal, Toronto' \
 "${D}"'Manitoba' \
 "${D}"'Alberta' \
 "${D}"'California, British Columbia' \
-"${S}"'UTC-10   Hawaii (no DST)' \
+"${S}"'UTC-10   Hawaii' \
 )
 
 TIMEZONE=( \
@@ -66,81 +66,98 @@ TIMEZONE=( \
 )
 
 
+KEY_AMAJOR='G1swOzMybVdoZW4gSSB3YXMgeW91bmdlciBzbyBtdWNoIHlvdW5nZXIgdGhhbiB0b2RheQpJIG5l
+dmVyIG5lZWRlZCBhbnlib2R5J3MgaGVscCBpbiBhbnkgd2F5CkJ1dCBub3cgdGhlc2UgZGF5cyBh
+cmUgZ29uZSwgSSdtIG5vdCBzbyBzZWxmIGFzc3VyZWQKTm93IEkgZmluZCBJJ3ZlIGNoYW5nZWQg
+bXkgbWluZCBhbmQgb3BlbmVkIHVwIHRoZSBkb29ycwobWzM1bXVzYWdlIGV4YW1wbGVzOiBbLXVd
+G1swOzBtCg=='
+
+
+## FUNCTIONS
 usage(){
   echo -e "Usage [-h]:"
-  printf "%-35b %b\n" "${T}- Hide commands [-n]:" "${C}${0} -n${X}"
-  printf "%-35b %b\n" "${T}- Show more commands [-m]:" "${C}${0} -m${X}"
-  printf "%-35b %b\n" "${T}- Refresh once a minute:"  "${C}watch -c -n 60 \"${0} -n\"${X}"
+  printf "%-35b %b\n" "${T}- Mini [-m]:" "${C}${0} -m${X}"
+  printf "%-35b %b\n" "${T}- Extended [-x]:" "${C}${0} -x${X}"
+  printf "%-35b %b\n" "${T}- Refresh once a minute:"  "${C}watch -c -n 60 \"${0} -m\"${X}"
   printf "%-35b %b\n" "${T}- Local date:"     "${C}date${X}"
+  printf "%-35b %b\n" "${T}- list timezones:"   "${C}timedatectl list-timezones | more${X}"
 }
 
-
-## Start program
-# Show or hide calculations [ 0 = minimal, 1 = default, 2 = extended]
-PRINT_CALC='1'               # Show Calculations
-
-
-# Getops
-while getopts ":hnmv" opt; do
-   case "${opt}" in
-     v) echo "gdate version: ${VERSION}" && exit 0;;  # version
-     h) usage && exit 0;;                             # usage / help
-     n) PRINT_CALC='0';;                              # Hide calculations with "-n" option
-     m) clear; PRINT_CALC='2';;                              # extra info with "-m" option
-     *) ;;
-   esac
-done
-
-
-# Header
-printf "%-59b %-10b\n"  "${T}Location:"                               "${T}${CURRENT_CITY}"   # 1 character offset due to color
-printf "%-60b %-10b\n"  "${M}Current time:"                           "$(TZ=${CURRENT_TIMEZONE[0]} date)"
-#echo
-printf "%-60b %-10b\n"  "${P}${NEXT_TZ_CHANGE[0]}" "${P}${NEXT_TZ_CHANGE[1]}"
-#printf "%-20b %-38b %-20b %b\n" "${S}DAYLIGHT TIME:" "$(TZ=${CURRENT_TIMEZONE[2]} date +'%r %Z')" "${S}STANDARD TIME:" "$(TZ=${CURRENT_TIMEZONE[1]} date +'%r %Z')"
-printf "%-20b %b\n" "${S}STANDARD TIME:" "$(TZ=${CURRENT_TIMEZONE[1]} date +'%r %Z')"
-printf "%-20b %b\n" "${S}DAYLIGHT TIME:" "$(TZ=${CURRENT_TIMEZONE[2]} date +'%r %Z')"
-echo -e "${BAR}"
-
+# Local Time
+local_time(){
+    printf "%-57b %-10b\n"  "${T}Location:"                               "${T}${CURRENT_CITY}"   # 1 character offset due to color
+    printf "%-58b %-10b\n"  "${M}Current time:  ------> ------->"      "$(TZ=${CURRENT_TIMEZONE[0]} date)"
+    if [ "${DISPLAY_PANELS}" -ge '1' ]; then
+        NEXT_TIME_CHANGE_IN_UTC=$(curl -s "http://worldtimeapi.org/api/timezone/America/Toronto" | cut -d ',' -f9 | cut -d '"' -f4)
+        if [ -n "${NEXT_TIME_CHANGE_IN_UTC}" ]; then
+	    NEXT_TZ_CHANGE[0]="Next Local Time change:"
+            NEXT_TZ_CHANGE[1]=$(TZ="${CURRENT_TIMEZONE[0]}" date -d "${NEXT_TIME_CHANGE_IN_UTC}")
+            printf "%-58b %-10b\n"  "${P}${NEXT_TZ_CHANGE[0]}" "${P}${NEXT_TZ_CHANGE[1]}"
+            printf "%-20b %b\n" "${S}STANDARD TIME:" "$(TZ=${CURRENT_TIMEZONE[1]} date +'%r %Z')"
+            printf "%-20b %b\n" "${S}DAYLIGHT TIME:" "$(TZ=${CURRENT_TIMEZONE[2]} date +'%r %Z')"
+	else
+            printf "%-20b %b\n" "${S}STANDARD TIME:" "$(TZ=${CURRENT_TIMEZONE[1]} date +'%r %Z')"
+            printf "%-20b %b\n" "${P}DAYLIGHT TIME:" "$(TZ=${CURRENT_TIMEZONE[2]} date +'%r %Z')"
+	fi
+    else	
+        printf "%-20b %b\n" "${S}STANDARD TIME:" "$(TZ=${CURRENT_TIMEZONE[1]} date +'%r %Z')"
+        printf "%-20b %b\n" "${P}DAYLIGHT TIME:" "$(TZ=${CURRENT_TIMEZONE[2]} date +'%r %Z')"
+    fi
+    echo -e "${BAR}"
+}
 
 # World Timezones display
-printf "%-71b %-40b %b\n"  "${T}Name                 ${P}(Yellow+Red = DST in summer)"  "${T}(Unix location)"  "TIME"
-i=0
-while [ $i -lt "${#NAME[@]}" ]; do
-    printf "%-65b %-20s %s\n"  "${NAME[$i]}" "${TIMEZONE[$i]}" "$(TZ=${TIMEZONE[$i]} date)"
-    i=$(( $i+1 ))
-done
-echo -e "${BAR}"
-
+world_time(){
+    printf "%-64b %-40b %b\n"  "${T}Name          ${P}(Yellow+Red = DST in summer)"  "${T}(Unix location)"  "TIME"
+    i=0
+    while [ $i -lt "${#NAME[@]}" ]; do
+        printf "%-58b %-20s %s\n"  "${NAME[$i]}" "${TIMEZONE[$i]}" "$(TZ=${TIMEZONE[$i]} date)"
+        i=$(( $i+1 ))
+    done
+    echo -e "${BAR}"
+}
 
 # Default features:
-
-# Date Calculation examples
-print_calculation_function(){
+print_calculation_syntax(){
     printf "%-35b %b\n\n" "${T}Time and date commands:"     "${P}  <<  Only change what is in the quotes >>${C}"
-    echo -e "${T}# It is 2pm EST (or EDT) in Montreal, what time is it in India? (IST)${C}"
-    printf "%-60s %s\n" 'TZ=Asia/Calcutta date -d "14:00 EST"'             "# EST to IST"
-    printf "%-60s %s\n" 'TZ=Asia/Calcutta date -d "1 Aug 2022 14:00 EST"'  "# EST (and optional date) to IST"
-    printf "%-60s %s\n" 'TZ=Asia/Calcutta date -d "14:00 EDT"'             "# EDT to IST"
-
+    echo -e "${T}# It is 2pm in Montreal, what time is it in India? (IST)${C}"
+    printf "%-58s %s\n" 'TZ=Asia/Calcutta date -d "14:00 EST"'              "# EST ---------> IST"
+    printf "%-58s %s\n" 'TZ=Asia/Calcutta date -d "1 Aug 2022 2:00 pm EST"' "# EST & date --> IST"
+    printf "%-58s %s\n" 'TZ=Asia/Calcutta date -d "14:00 EDT"'              "# EDT ---------> IST"
     echo
     echo -e "${T}# It is 2pm in India, what time is it in Montreal?${C}"
-    printf "%-60s %s\n" 'TZ=EST date -d "14:00 IST"'                          "# IST to EST"
-    printf "%-60s %s\n" 'TZ=EDT4 date -d "2:00 pm IST"'                       "# IST to EDT"
-    printf "%-60s %s\n" 'TZ=America/New_York date -d "1 Aug 2022 14:00 IST"'  "# IST to local Montreal time, (and optional date)"
+    printf "%-58s %s\n" 'TZ=EST date -d "14:00 IST"'                        "# IST ---------> EST"
+    printf "%-58s %s\n" 'TZ=EDT4 date -d "2:00 pm IST"'                     "# IST ---------> EDT"
+    printf "%-58s %s\n" 'TZ=America/Toronto date -d "1 Aug 2022 14:00 IST"' "# IST & date --> ET (local time)"
 }
 
-
-# Extended features [-m]:
+# Extended features:
 how_to_adjust(){
-  echo -e "\nThis script is easy to adjust:"
-  echo "1) Simply add or remove names of cities from the \"TIMEZONE\" varible"
-  printf "%-35b %b\n" "${T}   - To see a list of cities:"   "${C}timedatectl list-timezones | more${X}"
-  echo "2) and add or remove the corresponding entry in the \"NAME\" array."
-  echo "   - The \"NAME\" entry can be any name."
+    echo -e "\nThis script is easy to adjust:"
+    echo "1) Add or remove names of timzones from the \"TIMEZONE\" varible"
+    echo "2) And add or remove the corresponding entry in the \"NAME\" array."
+    echo "   - The \"NAME\" entry can be any name."
 }
 
+#------------------------------
+## Start program
+#[ 0 = time only, 1 = calculation syntax, 2 = extended]
+DISPLAY_PANELS='1'               # default
 
-[ "${PRINT_CALC}" -ge '1' ] && print_calculation_function
-[ "${PRINT_CALC}" == '1' ] && echo -e "${P}|==> No time/date commands (-n option)   |==>   more information (-m option)${C}"
-[ "${PRINT_CALC}" == '2' ] && echo -e "${BAR}" && usage && how_to_adjust
+# Getops
+while getopts ":vuhmx" opt; do
+    case "${opt}" in
+        v) echo "gdate version: ${VERSION}" && exit 0;;      # version
+        u) usage && exit 0;;                                 # usage
+        h) echo -e "${KEY_AMAJOR}" | base64 -d && exit 0;;   # help
+        m) DISPLAY_PANELS='0';;                              # minimal
+        x) clear; DISPLAY_PANELS='2';;                       # extended
+        *) ;;
+    esac
+done
+
+local_time
+world_time
+[ "${DISPLAY_PANELS}" -ge '1' ] && print_calculation_syntax
+[ "${DISPLAY_PANELS}" == '1' ] && echo -e "${P}|==> Mini [-m]       |==> Extended [-x]      |==> Usage [-h]${C}"
+[ "${DISPLAY_PANELS}" == '2' ] && echo -e "${BAR}" && usage && how_to_adjust
